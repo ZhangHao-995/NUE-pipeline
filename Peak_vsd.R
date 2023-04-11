ae73ed968af223af979ed16c5869663ba388376e
ZhangHao-995 <130420571+ZhangHao-995@users.noreply.github.com>
2023-04-11 12:59:15 +0800

    Create Peak_vsd.R

---
+++ Peak_vsd.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+library(DESeq2)
+countMat <- read.table("Peak_count.txt",header=T)
+col<-data.frame(condition=c("HN","LN","HN","LN"),
+cultivar=c("KN","KN","J4","J4"))
+rownames(col)<-colnames(countMat)
+dds<-DESeqDataSetFromMatrix(countData=countMat,
+ colData=col,
+ design=~cultivar+condition)
+dds = DESeq(dds)
+vsd<-vst(dds, blind=FALSE)
+count_norm<-assay(vsd)
+count_norm <- cbind(merge_peak,count_norm)
